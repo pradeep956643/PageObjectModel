431a2e1b0240a8b876c9c9adee8d776f4c3673b9
Pradeep956643 <pradeeprajendiren@gmail.com>
2019-11-12 11:50:45 +0530

    New method added

--- src/test/java/com/crm/qa/testcases/HomePageTest.java
+++ src/test/java/com/crm/qa/testcases/HomePageTest.java
@@ -21,11 +21,11 @@ public class HomePageTest extends TestBase {
 		super();
 	}
 
-	//test cases should be separated -- independent with each other
-	//before each test case -- launch the browser and login
-	//@test -- execute test case
-	//after each test case -- close the browser
-	
+	// test cases should be separated -- independent with each other
+	// before each test case -- launch the browser and login
+	// @test -- execute test case
+	// after each test case -- close the browser
+
 	@BeforeMethod
 	public void setUp() {
 		initialization();
@@ -34,33 +34,35 @@ public class HomePageTest extends TestBase {
 		loginPage = new LoginPage();
 		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
 	}
-	
-	
-	@Test(priority=1)
-	public void verifyHomePageTitleTest(){
+
+	@Test(priority = 1)
+	public void verifyHomePageTitleTest() {
 		String homePageTitle = homePage.verifyHomePageTitle();
-		Assert.assertEquals(homePageTitle, "CRMPRO","Home page title not matched");
+		Assert.assertEquals(homePageTitle, "CRMPRO", "Home page title not matched");
 	}
-	
-	@Test(priority=2)
-	public void verifyUserNameTest(){
+
+	@Test(priority = 2)
+	public void verifyUserNameTest() {
 		testUtil.switchToFrame();
 		Assert.assertTrue(homePage.verifyCorrectUserName());
 	}
-	
-	@Test(priority=3)
-	public void verifyContactsLinkTest(){
+
+	@Test(priority = 3)
+	public void verifyContactsLinkTest() {
 		testUtil.switchToFrame();
 		contactsPage = homePage.clickOnContactsLink();
 	}
-	
-	
-	
+
 	@AfterMethod
-	public void tearDown(){
+	public void tearDown() {
 		driver.quit();
 	}
-	
-	
+
+	@Test
+	public void newMethod() {
+
+		System.out.println(" New method created");
+
+	}
 
 }
